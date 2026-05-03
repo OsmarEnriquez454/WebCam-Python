f283c7769754c9f481ab4cf44280eef930f65e2d
Osmar Enriquez <osmarenriquez2006007@gmail.com>
2026-05-03 12:52:36 -0600

    Add files via upload

---
+++ camara.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO 
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error camara no disponble")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    results = model(frame)
+    annotated = results[0].plot()
+
+    cv2,imshow("Deteccion YOLO", annotated)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows
